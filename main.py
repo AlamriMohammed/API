52703dbb66c4136272dfddb35113d27fd4c85912
MohammedAlamri <m.s.alamri22@gmail.com>
2024-08-30 22:32:08 +0300

    test

--- main.py
+++ main.py
@@ -1,14 +1,61 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import requests
 
 app = FastAPI()
-
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return "Welcome To Tuwaiq Academy"
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+# url = "http://localhost:8000/me"
+# data = {
+# "Year": 2020,
+# "Engine_Size": 2.5,
+# "Mileage": 15000,
+# "Type": "Accent",
+# "Make": "Hyundai",
+# "Options": "Full"
+# }
+# response = requests.post(url, json=data)
+# print(response.json())
+
+
+import joblib
+# model = joblib.load('knn_model.joblib')
+model = joblib.load("Log_Reg (1).joblib")
+scaler = joblib.load("Scaler.joblib")
+
+
+from pydantic import BaseModel
+# Define a Pydantic model for input data validation
+class InputFeatures(BaseModel):
+    # highest_value: int
+        Mileage: int
+        Engine_Size: float
+
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+    # 'highest_value': input_features.highest_value
+        'Mileage': input_features.Mileage,
+        'Engine_Size': input_features.Engine_Size
+
+    }
+    feature_list = [dict_f[key] for key in sorted(dict_f)]
+    return scaler.transform([list(dict_f.values())])
+
+    # return features_list.value()
+
+@app.get("/predict")
+def predict(input_features: InputFeatures):
+    return preprocessing(input_features)
+
+@app.post("/predict")
+async def predict(input_features: InputFeatures):
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]}
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -X POST "https://usecase-7.onrender.com/predict" -H "Content-Type: application/json" \
+-d '{"age":23 , "current_value":50000000}'
